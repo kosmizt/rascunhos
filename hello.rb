5b1f0965ec4e741e54f1d1d974ab059d8a8de6e6
kosmizt <63089859+kosmizt@users.noreply.github.com>
2026-04-04 20:51:29 -0300

    Create hello.rb with string and array examples
    Add Ruby script demonstrating various string and array operations.

---
+++ hello.rb
@@ -0,0 +1,27 @@
+3.times do
+  print "Welcome "
+end
+
+puts 36/7
+puts 36.0/7
+
+print ("sample".reverse)
+print ("sample".length)
+print ("sample"*5)
+
+print (36.to_s.reverse)
+
+x = [12, 47, 35]
+print (x.max)
+print (x.sort)
+puts x[0]
+puts x[1]
+puts x[2]
+
+text = "O that we now had here
+But one ten thousand of those men in England
+That do no work to-day!"
+
+puts.gsub("England", "France")
+
+#3.2
