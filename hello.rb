99d43bfb1de31e9eb47ca75b1bcd9636f18d9416
kosmizt <63089859+kosmizt@users.noreply.github.com>
2026-04-04 22:37:17 -0300

    Replace print with puts for output statements

--- hello.rb
+++ hello.rb
@@ -5,15 +5,15 @@ end
 puts 36/7
 puts 36.0/7
 
-print ("sample".reverse)
-print ("sample".length)
-print ("sample"*5)
+puts ("sample".reverse)
+puts ("sample".length)
+puts ("sample"*5)
 
-print (36.to_s.reverse)
+puts (36.to_s.reverse)
 
 x = [12, 47, 35]
-print (x.max)
-print (x.sort)
+puts (x.max)
+puts (x.sort)
 puts x[0]
 puts x[1]
 puts x[2]
@@ -21,7 +21,7 @@ puts x[2]
 text = "O that we now had here
 But one ten thousand of those men in England
 That do no work to-day!"
-
-puts.gsub("England", "France")
+text.gsub!("England", "France")
+#puts text.gsub("England", "Italy")
 
 #3.2
